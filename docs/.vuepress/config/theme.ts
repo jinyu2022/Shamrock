07e8ce3a35643e9ecc7232131fb4976acee95d65
小坎坷 <723733393@qq.com>
2023-09-28 21:22:23 +0800

    docs: 添加 API 文档-用户相关 内容

--- docs/.vuepress/config/theme.ts
+++ docs/.vuepress/config/theme.ts
@@ -24,7 +24,7 @@ export default hopeTheme({
       text: 'API 文档',
       prefix: 'api',
       icon: 'book',
-      children: ['request-response', 'account']
+      children: ['request-response', 'account', 'user']
     }
   ]
 })
