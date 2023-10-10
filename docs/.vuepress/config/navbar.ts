9088b15a675ae2b97b918a3d4e47e0cfb842b19f
真心 <qgzhenxin@qq.com>
2023-10-10 21:10:11 +0800

    feat: 添加 交流群

--- docs/.vuepress/config/navbar.ts
+++ docs/.vuepress/config/navbar.ts
@@ -1,12 +1,5 @@
 import { navbar } from 'vuepress-theme-hope'
 
-const shamrockNavbar = navbar([
-  '/guide',
-  '/advanced',
-  '/api',
-  '/message',
-  '/event',
-  { text: 'QQ群', link: 'https://qm.qq.com/q/LlHABKWhu4', icon: 'fa-brands fa-qq' }
-])
+const shamrockNavbar = navbar(['/guide', '/advanced', '/api', '/message', '/event', '/group.md'])
 
 export { shamrockNavbar }
